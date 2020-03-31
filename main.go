5d9dcee8a6d324c6807d7808efea95810ccb8e7d
Nitin SINGH <nitinkumar.singh@f5.com>
2020-03-31 10:41:56 +0530

    intials

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// func main is just a function name but when used with package main, it serves as the application entry point.
+func main() {
+	fmt.Println("Welcome to educatify !")
+}
