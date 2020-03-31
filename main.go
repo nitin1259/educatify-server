907043a1d9937c1d0ec64db0dfd13ae361760e71
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-01 02:06:50 +0530

    adding the entry point of app

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
 
 // func main is just a function name but when used with package main, it serves as the application entry point.
 func main() {
 	fmt.Println("Welcome to educatify !")
+
+	router := mux.NewRouter()
+	// router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	//Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+
+	if err != nil {
+		fmt.Print(err)
+	}
 }
